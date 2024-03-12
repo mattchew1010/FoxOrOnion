2ae623913f74d3471373a76bd0f73343742e87f4
mattchew1010 <mattchewiscool@gmail.com>
2024-03-12 19:36:53 -0400

    Add storage to .gitignore, update package.json dependencies, and add crawler.js for web scraping

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import {v4 as uuidv4} from "uuid";
 const prisma = new PrismaClient();
-
+//####USERS####
 export async function CreateUser(email, password, username) {
   return prisma.user.create({
     data: {
@@ -13,4 +13,27 @@ export async function CreateUser(email, password, username) {
       uuid: uuidv4(),
     },
   })
+}
+
+
+
+//####ARTICLES####
+export async function createArticle(url, title, realNews){
+  if (realNews){
+    return prisma.news.create({
+      data: {
+        url: url,
+        title: title,
+        uuid: uuidv4(),
+      },
+    })
+  } else{
+    return prisma.satire.create({
+      data: {
+        url: url,
+        title: title,
+        uuid: uuidv4(),
+      },
+    })
+  }
 }
