1f2645893c097e1f851276b2703f402925a7181f
mattchew1010 <mattchewiscool@gmail.com>
2024-03-12 21:27:32 -0400

    Add Games component and update Games page

--- src/app/games/page.js
+++ src/app/games/page.js
@@ -1,7 +1,11 @@
-export default function Games(){
-return (
-   <div>
-      Tempoary Games Page
-   </div>
-)
+import {Game} from "@components/nav/games.js"
+
+export default function Games() {
+   return (
+      <div className="flex justify-center mt-10 xs:mt-2 gap-16">
+         <Game name="Classic" link="/games/classic" description='A regular News Or Not game. Just pick if you think the article is real or not.' disabled={false}/>
+         <Game name="Work In Progress" disabled={true}/>
+         <Game name="Work In Progress" disabled={true}/>
+      </div>
+   );
 }
