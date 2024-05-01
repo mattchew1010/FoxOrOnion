4d13c5a9aa149732a425ad13cf33c2c30dc7f693
mattchew1010 <mattchewiscool@gmail.com>
2024-05-01 13:41:52 -0400

    Add meta tags for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={metadata.title} />
+      <meta name="twitter:description" content={metadata.description} />
+      
+
       <head>
         <script defer src={process.env.UMAMI_URL} data-website-id={process.env.UMAMI_WEBSITEID}/>
       </head>
